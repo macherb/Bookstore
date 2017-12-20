7bc26b5a9d540c7e6b0ae4347ab6e3611178a805
Robert Macher <bobby_macher@hotmail.com>
2017-12-20 00:19:07 -0600

    Removed Server, Database, User Id and Password
    Removed Server, Database, User Id and Password from sqlConnection

---
+++ Bookstore/Data Access Layer/AccessDataSQLServer.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookstore
+{
+    /// <summary>
+    /// This public static class will be used to return a database connection object.
+    /// </summary>
+    class AccessDataSQLServer
+    {
+        #region Private variables
+
+        private string connectionString { get; set; }
+
+        #endregion
+
+        #region Public functions
+
+        /// <summary>
+        /// Returns a database connection from the SQLConnection class.
+        /// </summary>
+        public static SqlConnection GetConnection()
+        {
+            //You must instantiate the connection object with a supplied/valid connection string.
+            SqlConnection sqlConnection = new SqlConnection("");
+            return  sqlConnection;
+        }
+        
+        #endregion
+    }
+}
