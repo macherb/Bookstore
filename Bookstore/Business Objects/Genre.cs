138458d9139266875ed26c46d218db3fc9a82383
Robert Macher <bobby_macher@hotmail.com>
2017-12-20 00:10:11 -0600

    Create Genre.cs

---
+++ Bookstore/Business Objects/Genre.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookstore
+{
+    class Genre
+    {
+        #region Public variables
+
+        public int      id      { get; set; }
+        public string   name    { get; set; }
+
+        #endregion
+
+        #region Constructor
+
+        public Genre()
+        {
+            ;
+        }
+
+        #endregion
+    }
+}
