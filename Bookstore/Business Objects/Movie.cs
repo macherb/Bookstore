794b60fb8064d5201a72ef836e31c2bb21494fc3
Robert Macher <bobby_macher@hotmail.com>
2018-01-03 06:34:53 -0600

    New field for data gained when JOINed with Genre
    Created a new field for the data gained when it's JOIN'ed with Genre.

--- Bookstore/Business Objects/Movie.cs
+++ Bookstore/Business Objects/Movie.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,10 +11,11 @@ namespace Bookstore
         #region Public variables
 
         public int      movie_number        { get; set; }
-        public string   movie_title         { get; set; }
+        public string   movie_title         { get; set; }//TODO extra for another table
         public string   Description         { get; set; }
         public int      movie_year_made     { get; set; }
         public int      genre_id            { get; set; }
+        public string   name                { get; set; }//TODO needs to be special parameter (extra)
         public string   movie_rating        { get; set; }
         public string   media_type          { get; set; }
         public float    movie_retail_cost   { get; set; }
