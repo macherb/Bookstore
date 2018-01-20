449d2793e35bd6724a17035c733f331ad9f35a4d
Robert Macher <bobby_macher@hotmail.com>
2018-01-20 04:07:07 -0600

    New field for data gained when JOINed with Member
    Created a new field for the data gained when it's JOIN'ed with Member.

--- Bookstore/Business Objects/Rental.cs
+++ Bookstore/Business Objects/Rental.cs
@@ -14,6 +14,7 @@ namespace Bookstore
         public string   movie_title         { get; set; }//TODO needs to be special parameter (extra)
         public int      member_number       { get; set; }//TODO foreign
         public string   login_name          { get; set; }//TODO needs to be special parameter (extra)
+        public DateTime joindate            { get; set; }//TODO needs to be special parameter (extra)
         public DateTime media_checkout_date { get; set; }
         public DateTime media_return_date   { get; set; }
 
