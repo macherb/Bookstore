28f8a9a481b1458fc53857a983cdfcf42665d2af
Robert Macher <bobby_macher@hotmail.com>
2018-02-06 03:49:38 -0600

    Create variable str length Create variable ToolTip
    Created variables for length of the strings.
    Created variables for ToolTip.

--- Bookstore/Business Objects/Rental.cs
+++ Bookstore/Business Objects/Rental.cs
@@ -10,13 +10,18 @@ namespace Bookstore
     {
         #region Public variables
 
-        public int      movie_number        { get; set; }//TODO foreign
-        public string   movie_title         { get; set; }//TODO needs to be special parameter (extra)
-        public int      member_number       { get; set; }//TODO foreign
-        public string   login_name          { get; set; }//TODO needs to be special parameter (extra)
-        public DateTime joindate            { get; set; }//TODO needs to be special parameter (extra)
-        public DateTime media_checkout_date { get; set; }
-        public DateTime media_return_date   { get; set; }
+        public          int         movie_number        { get; set; }//TODO foreign
+        public          string      movie_title         { get; set; }//TODO needs to be special parameter (extra)
+        public          int         member_number       { get; set; }//TODO foreign
+        public          string      login_name          { get; set; }//TODO needs to be special parameter (extra)
+        public          DateTime    joindate            { get; set; }//TODO needs to be special parameter (extra)
+        public          DateTime    media_checkout_date { get; set; }
+        public          DateTime    media_return_date   { get; set; }
+
+        public static   string      movie_numberTip =       "movie unique identifier";
+        public static   string      member_numberTip =      "member unique number";
+        public static   string      media_checkout_dateTip ="date the media (dvd) was checked out";
+        public static   string      media_return_dateTip =  "date the media (dvd) was returned to store";
 
         #endregion
 
